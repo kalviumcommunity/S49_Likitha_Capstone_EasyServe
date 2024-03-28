3473fcd0ed73abb0a06256d45c6ed87abe444376
likitha1125 <likitha2511@gmail.com>
2024-03-28 17:48:10 +0530

    header created

---
+++ Frontend/client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx}', '.index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      colors:{
+        primary:'#F5385D',
+      },
+
+    },
+  },
+  plugins: [],
+}
